d4e5ce5c6b425bd8d1b3b2e15e6cde8554ab4d3a
Camilo <camiloagr2707@gmail.com>
2021-11-18 20:39:18 -0500

    Made outline for graph section

--- client/components/Graphs.jsx
+++ client/components/Graphs.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Graphs = () => {
-  return <div className='w-4/5 mx-auto pl-5 lg:pl-0'>Here goes the graphs</div>;
+  const [active, setActive] = useState('chart');
+  return (
+    <div className='w-4/5 mx-auto mt-5 pl-5 lg:pl-0'>
+      <header className='flex justify-between lg:w-1/3 md:w-96 mb-3'>
+        <button
+          className={`${
+            active === 'chart' ? 'bg-purple-600' : 'bg-purple-400'
+          } text-white rounded-xl px-3 py-1 md:px-3.5 md:py-2 hover:bg-purple-600 transform transition duration-500 hover:scale-105`}
+          onClick={() => setActive('chart')}
+        >
+          Chart
+        </button>
+        <button
+          className={`${
+            active === 'allocation' ? 'bg-purple-600' : 'bg-purple-400'
+          } text-white rounded-xl px-3 py-1 md:px-3.5 md:py-2 hover:bg-purple-600 transform transition duration-500 hover:scale-105`}
+          onClick={() => setActive('allocation')}
+        >
+          Allocation
+        </button>
+        <button
+          className={`${
+            active === 'statistics' ? 'bg-purple-600' : 'bg-purple-400'
+          } text-white rounded-xl px-3 py-1 md:px-3.5 md:py-2 hover:bg-purple-600 transform transition duration-500 hover:scale-105`}
+          onClick={() => setActive('statistics')}
+        >
+          Statistics
+        </button>
+      </header>
+      <section className='h-56 md:h-96 bg-yellow-800'></section>
+    </div>
+  );
 };
 
 export default Graphs;
--- client/components/HeaderSummary.jsx
+++ client/components/HeaderSummary.jsx
@@ -34,7 +34,7 @@ const HeaderSummary = () => {
                 d='M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
               />
             </svg>
-            <h3 className='text-sm'>Add new</h3>
+            <h3 className='text-sm'>Add New</h3>
           </button>
         </span>
       </span>
